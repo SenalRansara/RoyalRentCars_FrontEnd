722ab84009c4b64dda2ffa8c91a4b23a66601f75
Senal Ransara <senal.ransara38@gmail.com>
2022-04-15 10:04:26 +0530

    added routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,19 +1,41 @@
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+//Senal
 import RentalView from "../pages/rental/ViewRental";
 import AddRental from "../pages/rental/AddRental";
+
+// Chamoth
+
+
+//Ravindu
 import ReservationView from "../pages/reservation/ViewReservation";
 
+
+// Kaveen
 
 function AppRoutes() {
     return (
       <Router>
         <Header/>
       <Switch>
+        {/* Senal */}
         <Route path="/AllRental" exact component={RentalView} />
         <Route path="/AddRental" exact component={AddRental} />
+
+        {/* Chamoth */}
+
+
+
+        {/* Ravindu */}
         <Route path="/AllReservation" exact component={ReservationView} />
+
+
+
+        {/* Kaveen */}
+      
+  
+      
       </Switch>
         <Footer/>
       </Router>
